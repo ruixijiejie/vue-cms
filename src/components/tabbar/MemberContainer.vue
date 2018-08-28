8496cb34386e9a575063d02e4b9355e54b3d85f3
SoHyun <1819109014@qq.com>
2018-08-28 18:15:00 +0800

    项目第一天打卡

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>member</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MemberContainer.vue"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,31 @@
 // 入口文件
 import Vue from 'vue';
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
 // 导入mui样式
 import './lib/mui/css/mui.min.css'
-
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
 
 // 按需导入mint-UI中的组件
-import { Header } from 'mint-ui'
+import { Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem)
 // 导入app根组件
 import app from './App.vue'
 
 var vm = new Vue({
     el: '#app',
     render: c => c(app),
-
+    router   // 挂载路由对象到vm实例
 })
